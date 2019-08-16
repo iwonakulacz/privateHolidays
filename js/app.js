cf3928c1563cf5675255561eeccaa43fcefc975e
iwonakulacz <iwonakulacz@gmail.com>
2019-08-16 13:15:46 +0200

    add smooth scrool and fix mobile menu

--- js/app.js
+++ js/app.js
@@ -1,17 +1,40 @@
-const hamburger = document.querySelector('.nav__hamburger');
+const hamburger = document.querySelector(".nav__hamburger");
+const navbarMenu = document.querySelector(".nav__list");
+const navbarLinks = document.querySelectorAll(".nav__list a");
 
-function toggleMenu(e){
-    const show = hamburger.getAttribute('aria-expanded');
-    if(show === 'true'){
-        hamburger.setAttribute('aria-expanded', false);
-        $('.nav__list').removeClass('active');
-        $('.nav__hamburger').removeClass('active');
-    }
-    else{
-        hamburger.setAttribute('aria-expanded', true);
-        $('.nav__list').addClass('active');
-        $('.nav__hamburger').addClass('active');
-    }
+function navbarTogglerClick() {
+  hamburger.classList.toggle("active");
+  navbarMenu.classList.toggle("active");
 }
 
-hamburger.addEventListener('click', toggleMenu);
+function navbarLinkClick(event) {
+  smoothScroll(event);
+
+  if (navbarMenu.classList.contains("active")) {
+    hamburger.click();
+  }
+}
+
+function smoothScroll(event) {
+  event.preventDefault();
+  const targetId =
+    event.currentTarget.getAttribute("href") === "#"
+      ? "header"
+      : event.currentTarget.getAttribute("href");
+  document.querySelector(targetId).scrollIntoView({
+    behavior: "smooth",
+    block: "start"
+  });
+}
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", navbarLinkClick);
+}
+hamburger.addEventListener("click", navbarTogglerClick);
+window.addEventListener("resize", e => {
+  if (window.innerWidth > 991) {
+    if (navbarMenu.classList.contains("active")) {
+      hamburger.click();
+    }
+  }
+});
